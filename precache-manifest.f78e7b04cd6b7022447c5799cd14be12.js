ac9bb7d38a87ba26c09d850249c1a4e70feb4333
shualton <shualton@gmail.com>
2019-07-25 14:55:39 -0700

    Updates

---
+++ precache-manifest.f78e7b04cd6b7022447c5799cd14be12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc9f4bfc02c7842cc5d4c53f66dd29a",
+    "url": "/shualton/clicky-game.git/index.html"
+  },
+  {
+    "revision": "4a28d7dc9280cfcb0c28",
+    "url": "/shualton/clicky-game.git/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/shualton/clicky-game.git/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "4a28d7dc9280cfcb0c28",
+    "url": "/shualton/clicky-game.git/static/js/main.6097f470.chunk.js"
+  },
+  {
+    "revision": "1d558c4156a33120c179",
+    "url": "/shualton/clicky-game.git/static/js/runtime~main.9ad7fe79.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shualton/clicky-game.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
